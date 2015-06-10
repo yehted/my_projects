8378287add41de8a53363dfe297ebca274139ef3
Ted Yeh <tyeh@narrativescience.com>
2015-06-10 16:44:59 -0500

    finished is_happy

--- happy/happy.py
+++ happy/happy.py
@@ -9,32 +9,47 @@ def _calculate(num):
     return total
 
 def is_happy(num):
-    happy = {num: None, 1: None}
+    happy = {}
     initial = num
     while num not in happy:
+        happy[num] = None
         x = _calculate(num)
         if x == 1:
             return True
-        happy[x] = None
+        num = x
 
+    return False
 
-def main():
-    N = int(sys.argv[1])
+def happy_range(N):
     total = 0
-    happy = [False] * N
-    for initial in range(N):
+    happy = [False] * 10000
+    for initial in xrange(N):
         x = initial
         while not happy[x]:
             num = _calculate(x)
             if num == 1:
                 print 'happy number! ', initial
                 total += 1
-                happy = [False] * N
+                happy = [False] * 10000
                 break
             happy[x] = True
             x = num
 
     print total, "happy numbers"
+
+def main():
+    print "1. All the happy numbers in range"
+    print "2. Is this number happy?"
+    choice = raw_input("Choice: ")
+
+    N = int(sys.argv[1])
+
+    if choice == '1':
+        happy_range(N)
+    elif choice == '2':
+        print is_happy(N)
+    else:
+        print "invalid choice"
 
 if __name__ == '__main__':
     main()
