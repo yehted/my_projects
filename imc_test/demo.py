344471bf97f47e202772ae4b4fff488e93379413
Ted Yeh <tyeh@narrativescience.com>
2016-05-16 14:46:49 +0000

    explanation of imc demo problem

--- imc_test/demo.py
+++ imc_test/demo.py
@@ -1,5 +1,11 @@
+"""
+An equilibrium index is the index where all the previous values of the list add
+up to the sum of all the subsequent values of the list. In the example list A
+given below, equilibrium indices are at 1, 3, and 7
+"""
 A = [-1, 3, -4, 5, 1, -6, 2, 1]
 def solution(A):
+    result = [-1] * (len(A) + 1)
     Asum = sum(A)
     front = 0
     for i in range(1, len(A)):
@@ -10,9 +16,10 @@ def solution(A):
     #    print front
     #    print back
     #    print "---------------------"
-        if back == front:
-            return i
-    return -1
+#        if back == front:
+#            return i
+        result[i] = front == back
+    return result
 
 if __name__ == '__main__':
     print solution(A)
