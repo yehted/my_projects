eeaf10ecfe075db0ffef50494d51119dfae54691
Ted Yeh <tyeh@narrativescience.com>
2016-05-16 14:32:49 +0000

    working imc demo problem

--- imc_test/demo.py
+++ imc_test/demo.py
@@ -1,11 +1,18 @@
 A = [-1, 3, -4, 5, 1, -6, 2, 1]
-Asum = sum(A)
-front = 0
-for i in range(len(A)):
-    front = sum(A[:i])
-    # front = sum(A[:i])
-    # back = sum(A[i+1:])
-    back = Asum - front - A[i]
-    print front
-    print back
-    print "---------------------"
+def solution(A):
+    Asum = sum(A)
+    front = 0
+    for i in range(1, len(A)):
+        front += A[i - 1]
+        # front = sum(A[:i])
+        # back = sum(A[i+1:])
+        back = Asum - front - A[i]
+    #    print front
+    #    print back
+    #    print "---------------------"
+        if back == front:
+            return i
+    return -1
+
+if __name__ == '__main__':
+    print solution(A)
