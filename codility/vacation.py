9be99678cbd2fe7d54952e02c6065abd457e3da2
Ted Yeh <yeh.ted@gmail.com>
2016-05-18 19:13:17 -0500

    vacation code

--- codility/vacation.py
+++ codility/vacation.py
@@ -1,33 +1,42 @@
 A = [1, 1, 0, 1, 0, 0, 1, 1]
+B = [1, 1, 1, 0, 1, 0, 1, 0]
+
+
+def calculate(start, middle, end, A):
+    len_sea = middle - start + 0.0
+    len_mountains = end - middle + 0.0
+    total_mountains = sum(A)
+    sea = sum(A[start:middle]) / len_sea
+    mountains = sum(A[middle + 1: end + 1]) / len_mountains
+
+    return sea, mountains
 
 def solution(A):
+    print A
     sea = 0
     mountains = 0
     result = 0
+    sum_A = sum(A)
 
     start = 0
     end = len(A) - 1
+    middle = 1
 
-    total_mountains = sum(A)
-
-    while start != end:
-        temp_sum = sum(A[start:end + 1])
-        temp_len = end - start + 1.0
-
-        mountains = temp_sum / temp_len
-        sea = 1.0 - mountains
-        print mountains, sea
-
-        if mountains > 0.5 and sea > 0.5:
-            return temp_len
-        if sea <= 0.5:
+    front = 0.0
+    while start != middle and end != middle + 1:
+        front += A[middle]
+        if front / sum_A >= 0.5:
             start += 1
-        if mountains <= 0.5:
+        if (sum_A - front) / sum_A < 0.5:
             end -= 1
+        if front < sum_A:
+            middle += 1
+        else:
+            middle -= 1
 
-
-    return 0
+    print "---------------"
+    print A[start:end + 1]
+    return end - start + 1
 
 if __name__ == '__main__':
-    print "---------------"
     print solution(A)
